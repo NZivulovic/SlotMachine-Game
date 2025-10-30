ce241e2c1becefd2978f5cbe72275bab726edade
Nikola Zivulovic <104269774+NZivulovic@users.noreply.github.com>
2025-10-30 17:33:33 +0100

    Reward System Unfinished

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -3,4 +3,13 @@ extends Node
 
 var start_money = 1000
 var cost_money = 20
-var current_money
+var current_money = 0
+
+var seven_chance = 0.025
+var seven_reward_money = 100
+
+var star_chance = 0.20
+var star_reward_money = 40
+
+var sun_chance = 0.4
+var sun_reward_money = 100
--- Scripts/image_gen.gd
+++ Scripts/image_gen.gd
@@ -5,34 +5,51 @@ const star = preload("res://Assets/Star.jpg")
 const sun = preload("res://Assets/Sun.jpg")
 
 var textures = [seven, star, sun]
-var random_texture = textures[randi() % textures.size()]
+
+var rng = RandomNumberGenerator.new()
+var roll_chances = PackedFloat32Array([Global.seven_chance, Global.star_chance, Global.sun_chance])
 
 @onready var sprite_display: TextureRect = $TextureRect
-
-@onready var main_scene = load("res://Scenes/main.tscn")
+@onready var sprite_display1: TextureRect = $TextureRect1
+@onready var sprite_display2: TextureRect = $TextureRect2
 
 @onready var clickable_area: Area2D = %HandleBar
 
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready():
+	_randomize_texture()
 	clickable_area.input_event.connect(_on_clickable_area_input_event)
 	randomize()
 	
-	
+
 
 func _randomize_texture():
-	var chosen_texture = textures[randi() % textures.size()]
+	
+	var chosen_texture = textures[rng.rand_weighted(roll_chances)]
+	var chosen_texture1 = textures[rng.rand_weighted(roll_chances)]
+	var chosen_texture2 = textures[rng.rand_weighted(roll_chances)]
 	
 	if sprite_display:
 		sprite_display.texture = chosen_texture
+	if sprite_display1:
+		sprite_display1.texture = chosen_texture1
+	if sprite_display2:
+		sprite_display2.texture = chosen_texture2
+		
 
 
 func _on_clickable_area_input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			_randomize_texture()
+			check_for_reward()
+
+func check_for_reward():
+	if sprite_display.texture == sun and sprite_display1.texture == sun and sprite_display2.texture == sun:
+		Global.current_money += Global.sun_reward_money
+		print('sun')
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
